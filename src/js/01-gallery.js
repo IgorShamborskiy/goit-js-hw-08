546c6a8b74d9cafafdbb64395114af5284d4c9d6
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-12-19 18:09:05 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// console.log(galleryItems);
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
@@ -27,4 +28,4 @@ const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
 });
-console.log(galleryItems);
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const el = document.querySelector('iframe');
+const player = new Player(el);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+setCurrentTime();
+function setCurrentTime() {
+    if (!localStorage.getItem(STORAGE_KEY)) {
+        return;
+    }
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
