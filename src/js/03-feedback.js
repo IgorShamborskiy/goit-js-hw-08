ebb420e91a21e6989d38bcbc996e21079749f903
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-12-20 15:19:32 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,23 @@
 import throttle from 'lodash.throttle';
 
 const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
 const refs = {
     form: document.querySelector(`.feedback-form`),
     input: document.querySelector(`.feedback-form input`),
     textarea: document.querySelector(`.feedback-form textarea`),
 };
+
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener(`input`, throttle(onTexteriaInput, 500));
-const STORAGE_KEY = 'feedback-form-state';
+
 
 populateTextaria();
 
 
-refs.form.addEventListener(`input`, e => {
-    formData[e.target.name] = e.target.value;
+refs.form.addEventListener(`input`, evt => {
+    formData[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 
@@ -30,9 +33,11 @@ function onTexteriaInput(evt) {
 
 }
 function populateTextaria() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const savedMessage =localStorage.getItem(STORAGE_KEY);
     if (savedMessage) {
+        refs.input.value = savedMessage;
         refs.textarea.value = savedMessage;
     }
    
 }
+
